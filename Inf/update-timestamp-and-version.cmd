af240fa3216dd763cd3bb0f89a8bdd075523c773
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-06 22:30:32 +1100

    Update .cmd file.

--- Inf/update-timestamp-and-version.cmd
+++ Inf/update-timestamp-and-version.cmd
@@ -6,18 +6,13 @@ rem Remove old build products - cat invalidated by timestamp change.
 del *.cat *.htm > nul
 
 rem Update the date to today and the version to 7.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_cdc.inf -d * -v 7.%DRIVER_VER%
-
-rem Update the date to today and the version to 1.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_hdk_display.inf -d * -v 1.%DRIVER_VER%
+"%WDK_DIR%\bin\x86\stampinf" -f h2u_opsis_cap.inf -d * -v 7.%DRIVER_VER%
 
 rem Update the date to today and the version to 7.whateveryouentered
-rem (see inf as to why we need a high major version)
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_hdk_hid.inf -d * -v 7.%DRIVER_VER%
+"%WDK_DIR%\bin\x86\stampinf" -f h2u_opsis_con.inf -d * -v 7.%DRIVER_VER%
 
 rem Update the date to today and the version to 7.whateveryouentered
-rem (see inf as to why we need a high major version)
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_hdk_ircam.inf -d * -v 7.%DRIVER_VER%
+"%WDK_DIR%\bin\x86\stampinf" -f tofe_lowspeedio.inf -d * -v 7.%DRIVER_VER%
 
 popd
 pause
