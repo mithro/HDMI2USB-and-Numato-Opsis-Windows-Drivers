67c1d91db0983405f7084976582f8a98cdec948f
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-12 19:48:12 +1100

    Creating the config.cmd

---
+++ config.cmd
@@ -0,0 +1,5 @@
+set CERTNAME=Numato Lab
+set CROSS_CERT=DigiCert_Assured_ID_Root_CA.crt
+set TIMESTAMP_SERVER=http://timestamp.digicert.com
+set WDK_DIR=%ProgramFiles(x86)%\Windows Kits\10
+set NSIS_DIR=%ProgramFiles(x86)%\NSIS
